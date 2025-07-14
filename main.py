6ba73a33660578f2671adf646f556a169551d670
Umang Aggarwal <162720103+umang23567@users.noreply.github.com>
2025-07-15 05:05:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from utils.video_utils import read_video,save_video
 from trackers import Tracker
+import os
 
 def main():
     
@@ -8,6 +9,7 @@ def main():
 
     #Initialize tracker
     tracker = Tracker('model.pt')
+    os.makedirs('stubs', exist_ok=True)
     tracks=tracker.get_object_tracks(video_frames,
                                      read_from_stub=True,
                                      stub_path='stubs/track_stubs.pkl')
@@ -16,6 +18,7 @@ def main():
     #Draw object tracks
     output_video_frames=tracker.draw_annotations(video_frames,tracks)
     
+    os.makedirs('output_videos', exist_ok=True)
     #Save video
     save_video(output_video_frames,'output_videos/output.mp4')
     
